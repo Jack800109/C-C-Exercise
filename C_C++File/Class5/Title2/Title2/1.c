a9a94a8dbe5f79ff3d39957bb502dffd5f019a04
Jack800109 <55128457+Jack800109@users.noreply.github.com>
2021-02-21 11:15:40 +0800

    21/2/21 11clock

--- C_C++File/Class5/Title2/Title2/1.c
+++ C_C++File/Class5/Title2/Title2/1.c
@@ -4,11 +4,23 @@
 int main() {
 	char c[100];
 	int num = 0, length = 0;
+	char a;
 
 	scanf("%s", c);
 	length = sizeof(c)/sizeof(c[0]);
-	printf("%d\n", length);
-	printf("%s\n", c);
+	printf("数组长度为：%d\n", length);
+	printf("数组：%s\n", c);
+	for (int i = 0; i < length ; i++)
+	{
+		a = (char)(c[i]);
+		printf("%c\n",a);
+		//printf("int = %d\n", sizeof(a));
+		if (a == "A") {
+			num += 1;
+		}
+		//printf("num = %d\n", num);
+	}
+	printf("数组中A有%d个。\n", num);
 	system("pause");
 	return 0;
 }
