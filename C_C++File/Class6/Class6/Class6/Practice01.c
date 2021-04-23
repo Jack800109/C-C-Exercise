5b93b36e708c9e13894fbef3dfa415ea36ab38d6
Jack800109 <55128457+Jack800109@users.noreply.github.com>
2021-04-23 18:37:11 +0800

    Class6 update

---
+++ C_C++File/Class6/Class6/Class6/Practice01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum() {
+	int i, sum = 0;
+	for (i = 1; i <= 100; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
+int main() {
+	int a = sum();
+	printf("The sum is %d\n", a);
+	system("Pause");
+	return 0;
+}
