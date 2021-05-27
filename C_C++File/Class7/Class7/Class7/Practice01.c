88fd6a5a5faaf660e4d006bfd6bc24eec7628b3b
Jack800109 <55128457+Jack800109@users.noreply.github.com>
2021-05-27 21:53:01 +0800

    20210527

---
+++ C_C++File/Class7/Class7/Class7/Practice01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STR(s) #s
+int main() {
+	printf("%s\n", STR(c.biancheng.net));
+	printf("%s\n", STR("c.biancheng.net"));
+	system("pause");
+	return 0;
+	
+}
