d8a8e45fa9db12c82d198ba7dd97937824cb242a
Jack800109 <55128457+Jack800109@users.noreply.github.com>
2021-06-05 21:33:05 +0800

    class8

---
+++ C_C++File/Class8/Class8/Class8/Practice01.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+	char str[20] = "c.biancheng.net";
+	char c1 = str[4];
+
+	long num2 = str;
+
+	printf("c1 = %c\n", c1);
+	printf("num2 = %1d\n");
+	system("pause");
+	return 0;
+}
